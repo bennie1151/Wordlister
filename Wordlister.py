16574799b532bb9925ee7962541ee2c60c99819e
Anonymous <52291420+AnonymousFromGeorgia@users.noreply.github.com>
2020-05-02 10:47:04 +0400

    Update Wordlister.py

--- Wordlister.py
+++ Wordlister.py
@@ -45,7 +45,7 @@ if ask == 'კი':
 	report = open('wordlist.txt', 'w')
 
 
-	for target in (name,lname,bgfriendN,bgfriendL,byear,bmonth,bday,bday2,color,pet,country,city):
+	target in (name,lname,bgfriendN,bgfriendL,byear,bmonth,bday,bday2,color,pet,country,city):
 		report.write(name + '\n')
 		report.write(lname + '\n')
 		report.write(name+lname + '\n')
